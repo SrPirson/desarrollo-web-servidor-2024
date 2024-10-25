e4a54e6aa069ecac5684b9c81c0aa32dd3afd915
SrPirson <fcp0009@alu.medac.com>
2024-10-25 14:29:54 +0200

    Clase 25/10/2024

--- 04-formularios/irpf.php
+++ 04-formularios/irpf.php
@@ -5,41 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IRPF</title>
     <?php
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
 
-    require("../05-funciones/irpf.php");
+        require("../05-funciones/irpf.php");
     ?>
+    <style>
+        .error {
+            color: Tomato;
+            font-style: italic;
+        }
+    </style>
 </head>
 <body>
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $tmp_salario = $_POST["salario"];
+
+            if($tmp_salario == '') {
+                $error_salario = "El salario es obligatorio";
+            } else {
+                if(filter_var($tmp_salario, FILTER_VALIDATE_FLOAT) === FALSE) {
+                    $error_salario = "El salario debe ser un número";
+                } else {
+                    if($tmp_salario < 0) {
+                        $error_salario = "El salario debe ser mayor o igual que cero";
+                    } else {
+                        $salario = $tmp_salario;
+                    }
+                }
+            }
+        }
+    ?>
+
     <form action="" method="post">
         <input type="text" name="salario" placeholder="Salario">
+        <?php 
+            if(isset($error_salario)) {
+                echo "<span class='error'> $error_salario</span>";
+            } 
+        ?>
+        <br><br>
         <input type="submit" value="Calcular salario bruto">
     </form>
 
     <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $tmp_salario = $_POST["salario"];
-
-        if($tmp_salario == '') {
-            echo "<p>El salario es obligatorio</p>";
-        } else {
-            if(filter_var($tmp_salario, FILTER_VALIDATE_FLOAT) === FALSE) {
-                echo "<p>El salario debe ser un número</p>";
-            } else {
-                if($tmp_salario < 0) {
-                    echo "<p>El salario debe ser mayor o igual que cero</p>";
-                } else {
-                    $salario = $tmp_salario;
-                }
-            }
-        }
-
         if(isset($salario)) {
             $salario_final = calcularIRPF($salario);
             echo "<h1>El salario neto de $salario es $salario_final</h1>";    
         }
-    }
     ?>
 </body>
 </html>
--- 04-formularios/iva.php
+++ 04-formularios/iva.php
@@ -10,56 +10,92 @@
 
         require('../05-funciones/iva.php');
     ?>
+    <style>
+        .error {
+            color: lime;
+            font-style: italic;
+        }
+    </style>
 </head>
 <body>
+
+    <?php
+    /* Declaro todo arriba para detectar los errores */
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            /* Controlar precio */
+            $tmp_precio = $_POST["precio"];
+            if (isset($_POST["iva"])) {
+                $tmp_iva = $_POST["iva"];
+            } else {
+                $tmp_iva = "";
+            }
+
+            if($tmp_precio == '') {
+                $error_precio = "El precio es obligatorio";
+            } else {
+                if(filter_var($tmp_precio, FILTER_VALIDATE_FLOAT) === FALSE) {
+                    $error_precio = "El precio debe ser un número";
+                } else {
+                    if($tmp_precio < 0) {
+                        $error_precio = "El precio debe ser mayor o igual que cero";
+                    } else {
+                        $precio = $tmp_precio;
+                    }
+                }
+            }
+
+            /* Controlar el iva */
+            if($tmp_iva == '') {
+                $error_iva =  "El IVA es obligatorio";
+            } else {
+                $valores_validos_iva = ["general", "reducido", "superreducido"];
+                if(!in_array($tmp_iva, $valores_validos_iva)) { // Ver si el elemento no está en el array
+                    $error_iva = "El IVA solo puede ser: general, reducido, superreducido";
+                } else {
+                    $iva = $tmp_iva;
+                }
+            }
+        }
+    ?>
+
     <form action="" method="post">
         <label for="precio">Precio</label>
         <input type="text" name="precio" id="precio">
+        <!-- Mostramos el error en caso de que haya -->
+        <?php 
+            if(isset($error_precio)) {
+                echo "<span class='error'> $error_precio</span>";
+            } 
+        ?>
         <br><br>
         <select name="iva">
+            <!-- 
+            option disabled selected hidden
+            disabled - para no poder seleccionarlo
+            selected - para que aparezca seleccionada por defecto
+            hidden   - para que no aparezca en el desplegable
+            -->
+            <option disabled selected hidden>--- Elige un tipo de IVA ---</option>
+
             <option value="general">General</option>
             <option value="reducido">Reducido</option>
             <option value="superreducido">Superreducido</option>
         </select>
+        <?php 
+            if(isset($error_iva)) {
+                echo "<span class='error'> $error_iva</span>";
+            } 
+        ?>
         <br><br>
         <input type="submit" value="Calcular">
     </form>
 
     <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $tmp_precio = $_POST["precio"];
-        $tmp_iva = $_POST["iva"];
-
-        if($tmp_precio == '') {
-            echo "<p>El precio es obligatorio</p>";
-        } else {
-            if(filter_var($tmp_precio, FILTER_VALIDATE_FLOAT) === FALSE) {
-                echo "<p>El precio debe ser un número</p>";
-            } else {
-                if($tmp_precio < 0) {
-                    echo "<p>El precio debe ser mayor o igual que cero</p>";
-                } else {
-                    $precio = $tmp_precio;
-                }
-            }
-        }
-
-        if($tmp_iva == '') {
-            echo "<p>El IVA es obligatorio</p>";
-        } else {
-            $valores_validos_iva = ["general", "reducido", "superreducido"];
-            if(!in_array($tmp_iva, $valores_validos_iva)) { // Ver si el elemento no está en el array
-                echo "<p>El IVA solo puede ser: general, reducido, superreducido</p>";
-            } else {
-                $iva = $tmp_iva;
-            }
-        }
-
         if(isset($precio) && isset($iva)) {
-            echo calcularPVP($precio, $iva);
+            echo "<br>";
+            echo "<h1>El PVP es " . calcularPVP($precio, $iva) . "</h1>";
         }
-
-    }
     ?>
 </body>
 </html>
