b9014aa347274c8d662e25dd0f7ad981f563adbb
SrPirson <fcp0009@alu.medac.com>
2024-10-30 11:17:11 +0100

    Clase 30/10/2024

--- 04-formularios/usuario.php
+++ 04-formularios/usuario.php
@@ -23,14 +23,21 @@
 <body>
     <div class="container">
         <h1>Formulario Usuario</h1>
-        <br><br><br>
+        <br>
+        <h4>Fecha actual: <?php echo date("d-m-Y") ?></h4>
+        <br><br>
 
         <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $tmp_usuario = $_POST["usuario"];
                 $tmp_nombre = $_POST["nombre"];
                 $tmp_apellidos = $_POST["apellidos"];
+                $tmp_dni = $_POST["dni"];
+                $tmp_correo = $_POST["correo"];
+                $tmp_fechaNac = $_POST["fechaNac"];
 
+
+                /* Validación usuario */
                 if ($tmp_usuario == "") {
                     $err_usuario = "El usuario es obligatorio.";
                 } else {
@@ -43,6 +50,8 @@
                     }
                 }
 
+
+                /* Validación nombre */
                 if ($tmp_nombre == "") {
                     $err_nombre = "El nombre es obligario";
                 } else {
@@ -59,6 +68,8 @@
                     }
                 }
 
+
+                /* Validación apellido */
                 if ($tmp_apellidos == "") {
                     $err_apellidos = "El apellido es obligario";
                 } else {
@@ -75,6 +86,126 @@
                     }
                 }
 
+
+                /* Validación DNI */
+                if ($tmp_dni == "") {
+                    $err_dni = "El DNI es obligatorio";
+                } else {
+                    $tmp_dni = strtoupper($tmp_dni);
+                    $patron_dni = "/^[0-9]{8}[A-Z]$/";
+                    if (!preg_match($patron_dni, $tmp_dni)) {
+                        $err_dni = "El DNI debe tener 8 dígitos y 1 letra";
+                    } else {
+                        // substr espedificamos el rango que queremos guardar
+                        // en la variable tmp_dni empieza en la posición 0 y crea un array de 8 (0-7)
+                        $numero_dni = (int)substr($tmp_dni,0,8);
+                        $letra_dni = substr($tmp_dni,8,1);
+
+                        $resto_dni = $numero_dni % 23;
+                        
+                        $letra_correcta = match($resto_dni) {
+                            0 => "T",
+                            1 => "R",
+                            2 => "W",
+                            3 => "A",
+                            4 => "G",
+                            5 => "M",
+                            6 => "Y",
+                            7 => "F",
+                            8 => "P",
+                            9 => "D",
+                            10 => "X",
+                            11 => "B",
+                            12 => "N",
+                            13 => "J",
+                            14 => "Z",
+                            15 => "S",
+                            16 => "Q",
+                            17 => "V",
+                            18 => "H",
+                            19 => "L",
+                            20 => "C",
+                            21 => "K",
+                            22 => "E"
+                        };
+                        
+
+                        /* 
+                        Otra forma de hacerlo 
+                        Guardamos todas las letras del DNI en una variable
+                        Hacemos un substr de las letras en la posición del resto de los números
+                        De esta manera tenemos en el array solamente la letra correcta
+                        
+                        $letra_dni = "TRWAGMYFPDXBNJZSQVHLCKE";
+                        $letra_correcta = substr($letra_dni, $resto_dni, 1);
+                        */
+                        if ($letra_dni != $letra_correcta) {
+                            $err_dni = "La letra del DNI no es correcta";
+                        } else {
+                            $dni = $tmp_dni;
+                        }
+                    }
+                }
+
+
+                /* Validación correo electronico */
+                if ($tmp_correo == "") {
+                    $err_correo = "El correo electrónico es obligatorio.";
+                } else {
+                    $patron_correo = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+                    if (!preg_match($patron_correo, $tmp_correo)) { 
+                       $err_correo = "El correo electrónico no es válido."; 
+                    } else {
+                        $palabras_baneadas = ["caca", "peo", "recorcholis", "caracoles", "repampanos"];
+                        $palabras_encontradas = "";
+                        foreach ($palabras_baneadas as $palabra_baneada) {
+                            if (str_contains($tmp_correo, $palabra_baneada)) {
+                                $palabras_encontradas = "$palabra_baneada, $palabras_encontradas";
+                            }
+                            if ($palabras_encontradas != "") {
+                                $err_correo = "No se permiten las palabras: $palabras_encontradas";
+                            } else {
+                                $correo = $tmp_correo;
+                            }
+                        }
+                    }
+                }
+
+
+                /* Validación fecha nacimiento */
+                if ($tmp_fechaNac == "") {
+                    $err_fechaNac = "La fecha de nacimiento es obligatorio.";
+                } else {
+                    $patron_fecha = "/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/";
+                    if (!preg_match($patron_fecha, $tmp_fechaNac)) {
+                        $err_fechaNac = "El formato de la fecha no es correcta.";
+                    } else {
+                        $fechaActual = explode("-", date("Y-m-d"));
+                        // list($anno_actual,$mes_actual,$dia_actual) = explode("-", date("Y-m-d"));
+                        $nacimiento = explode("-", $tmp_fechaNac);
+                        // list($anno,$mes,$dia) = explode("-", $tmp_fechaNac);
+
+                        if (($fechaActual[0] - $nacimiento[0]) > 18) {
+                            $fechaNac = $tmp_fechaNac;
+                        } elseif (($fechaActual[0] - $nacimiento[0]) < 18) {
+                            $err_fechaNac = "Eres menor de edad";
+                        } else {
+                            if (($fechaActual[1] - $nacimiento[1]) > 0) {
+                                $fechaNac = $tmp_fechaNac;
+                            } elseif (($fechaActual[1] - $nacimiento[1]) < 0) {
+                                $err_fechaNac = "Eres menor de edad";
+                            } else {
+                                if (($fechaActual[2] - $nacimiento[2]) >= 0) {
+                                    $fechaNac = $tmp_fechaNac;
+                                } elseif (($fechaActual[2] - $nacimiento[2]) < 0) {
+                                    $err_fechaNac = "Eres menor de edad";
+                                }
+                            }
+                        }
+                    } 
+                }
+
+                
             }
         ?>
 
@@ -100,7 +231,6 @@
             <div class="input-group mb-3">
                 <span class="input-group-text">Usuario</span>
                 <input type="text" class="form-control" name="usuario">
-                <button class="btn btn-outline-info" value="Enviar" type="submit">Enviar</button>
                 <?php 
                 if(isset($err_usuario)){
                     echo "<span class='error'>$err_usuario</span>";
@@ -108,7 +238,52 @@
                 ?>
             </div>
 
+            <div class="input-group mb-3">
+                <span class="input-group-text">DNI</span>
+                <input type="text" class="form-control" name="dni">
+                <?php 
+                if(isset($err_dni)){
+                    echo "<span class='error'>$err_dni</span>";
+                } 
+                ?>
+            </div>
+
+            <div class="input-group mb-3">
+                <span class="input-group-text">Correo electrónico</span>
+                <input type="text" class="form-control" name="correo">
+                <?php 
+                if(isset($err_correo)){
+                    echo "<span class='error'>$err_correo</span>";
+                } 
+                ?>
+            </div>
+
+            <div class="input-group mb-3">
+                <span class="input-group-text">Fecha nacimiento</span>
+                <input type="DATE" class="form-control" name="fechaNac">
+                <?php 
+                if(isset($err_fechaNac)){
+                    echo "<span class='error'>$err_fechaNac</span>";
+                } 
+                ?>
+            </div>
+
+            <button class="btn btn-outline-info" value="Enviar" type="submit">Enviar</button>
+
         </form>
+
+        <?php
+            if (isset($dni) && isset($correo) && isset($apellidos) && isset($usuario) && isset($nombre) && isset($fechaNac)) {
+        ?>
+                <h1><?php echo $nombre ?></h1>
+                <h1><?php echo $apellidos ?></h1>
+                <h1><?php echo $dni ?></h1>
+                <h1><?php echo $usuario ?></h1>
+                <h1><?php echo $correo ?></h1>
+                <h1><?php echo "Eres mayor de edad" ?></h1>
+        <?php
+            }
+        ?>
 
     </div>
 
