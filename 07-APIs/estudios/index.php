8287a2bfcb8484987c69ff1e01fde529570756f5
SrPirson <fcp0009@alu.medac.com>
2025-01-22 09:40:53 +0100

    Clase 22/01/2025

---
+++ 07-APIs/estudios/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudios</title>
+    <?php
+        error_reporting( E_ALL );
+        ini_set("display_errors", 1 );
+    ?>
+</head>
+<body>
+    <?php 
+    
+        // Configurar la conexion de a la API
+        $apiUrl = "http://localhost/Ejercicios/07-APIs/estudios/api_estudios.php";
+        $curl = curl_init(); // Inicializamos la libreria cUrl
+        curl_setopt($curl, CURLOPT_URL, $apiUrl); // Indicamos que la conexion va por URL e indicamos la URL
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // Para habilitar la transferencia de datos
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+
+        // Pasamos los datos a un array JSON
+        $estudios = json_decode($respuesta, true);
+        // print_r($estudios);
+
+    ?>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Estudio</th>
+                <th>Ciudad</th>
+                <th>Año de fundación</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($estudios as $estudio) { ?>
+                <tr>
+                    <th><?php echo $estudio["nombre_estudio"] ?></th>
+                    <th><?php echo $estudio["ciudad"] ?></th>
+                    <th><?php echo $estudio["anno_fundacion"] ?></th>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
