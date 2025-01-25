5fbc6bddd5cdd378b8a0e60f47c6f5e6767b76c4
SrPirson <169342716+SrPirson@users.noreply.github.com>
2025-01-25 11:17:28 +0100

    Casa 25/01/2025

--- 07-APIs/jikan/top_anime.php
+++ 07-APIs/jikan/top_anime.php
@@ -8,6 +8,7 @@
         error_reporting( E_ALL );
         ini_set("display_errors", 1 );
     ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <?php 
@@ -27,11 +28,37 @@
 
     ?>
 
-    <ol>
-        <?php
-        foreach ($animes as $anime) { ?>
-            <li><?php echo $anime["title"] ?></li>
-        <?php } ?>
-    </ol>
+    <table class="table text-center table-bordered border-secundary table-hover table-light">
+        <thead class="table-dark">
+            <tr>
+                <th>Titulo</th>
+                <th>Nota</th>
+                <th>Imagen</th>
+                <th>Trailer</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <?php
+                foreach ($animes as $anime) {
+                    echo "<tr>";
+                    echo "<td class='table-primary'>" . $anime["title"] . "</td>";
+                    echo "<td class='table-success'>" . $anime["score"] . "</td>";
+             ?>
+                <td class='table-secondary'>
+                    <img src="<?php echo $anime["images"]["jpg"]["image_url"] ?>" alt="<?php echo $anime["title"] ?>">
+                </td>
+                
+                <td class='table-secondary'>
+                    <iframe width="560" height="315" src="<?php echo $anime["trailer"]["embed_url"] ?>" title="<?php echo $anime["title"] ?>"></iframe>
+                </td>
+            <?php 
+                    echo "</tr>";
+                }   
+            ?>
+        </tbody>
+    </table>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
